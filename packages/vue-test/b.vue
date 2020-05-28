eca76c6bd6c6f48e51f5924a8772f840d11e8f11
Tommy Duong <tommyduong23@gmail.com>
2020-05-27 19:19:35 -0700

    test

---
+++ packages/vue-test/b.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  .b
+    h1 Test {{ test }}
+</template>
+
+<script>
+export default {
+  name: 'b',
+
+  data: () => ({
+    test: 'b',
+  }),
+};
+</script>
+
+<style lang="scss">
+
+</style>
